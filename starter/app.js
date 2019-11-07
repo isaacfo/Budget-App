47a54496d90bafd651d7e29f228a536a32639797
Isaac Fonseca Lopez <isaacfo1010@gmail.com>
2019-11-07 13:00:52 -0500

    added clearfields function

--- starter/app.js
+++ starter/app.js
@@ -71,7 +71,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     }
 
     return {
@@ -86,12 +88,39 @@ var UIController = (function() {
         },
 
         addListItem: function(obj, type) {
-            var html;
+            var html, newHtml, element;
             // Create HTML string with placeholder text
-            html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-            // Replace the placejplder text with some actual data
 
+            if(type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expenseContainer
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            
+
+            
+            // Replace the placejplder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
             // insert HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue); 
+            
+             fieldsArr = Array.prototype.slice.call(fields);
+
+             fieldsArr.forEach(function(current, index, array) {
+                 current.value = "";
+             });
+
+             fieldsArr[0].focus()
 
         },
 
@@ -139,10 +168,15 @@ var controller = (function(budgetCtrl, UICtrl) {
        var newItem = budgetController.addItem(input.type, input.description, input.value);
 
        // 3. Add the item to the UI
+       UICtrl.addListItem(newItem, input.type);
 
-       // 4. Calculate the budget
+       // 4. Clear the fields
 
-       // 5. Display the budger on the UI
+       UICtrl.clearFields();
+
+       // 5. Calculate the budget
+
+       // 6. Display the budger on the UI
         
 
     };
